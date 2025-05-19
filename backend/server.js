57d819c10f61a4c397cf9992addd5f364bbc45af
kay-sndg <kaylynn@open.gov.sg>
2025-05-19 13:06:52 +0000

    edited package.json

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/api/food', foodRoutes);
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
